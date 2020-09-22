5c410078e471bb6153153b20a92311addd6dd544
Pierre FILSTROFF <pfilstroff@gmail.com>
2020-09-22 19:38:45 +0200

    Small changes

--- start.sh
+++ start.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
+PROJECT=$PROJECT
 ORGANIZATION=$ORGANIZATION
-ACCESS_TOKEN=$ACCESS_TOKEN
-
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$REG_TOKEN
 
 cd /home/docker/actions-runner
-
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+./config.sh --url https://github.com/${ORGANIZATION}/${PROJECT} --token ${REG_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
